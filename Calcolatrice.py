b72076a231ab669e256bd7e5fbf869872c86c6ff
Armir Trupja <ICTS22-24.351@studenti.local>
2023-03-30 11:33:04 +0200

    finale

--- Calcolatrice.py
+++ Calcolatrice.py
@@ -1,1 +1,7 @@
-
+new_action = input("\nDesideri continuare ad utilizzare l'Applicazione? S/N ")
+    if new_action == "S" or new_action == "s":
+        print("Sto tornando al menù principale!\n")
+        continue
+    else:
+        print("A presto!\n")
+        break
